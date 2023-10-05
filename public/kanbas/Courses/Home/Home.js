6a8174d7e4ae55ec4afdb7bb17f4a36e0dc27c7d
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-10-05 19:54:46 -0400

    made initial header bar

--- public/kanbas/Courses/Home/Home.js
+++ public/kanbas/Courses/Home/Home.js
@@ -12,6 +12,24 @@ function Home() {
           ${KanbasNavigation("Courses")}
         </div>
       </div>
+      <div class="main-content-wrapper">
+        <div class="header-bar">
+          <i class="fa-solid fa-bars" id="menu-button"></i>
+          <div class="breadcrumb-wrapper">
+            <nav id="breadcrumbs">
+              <ul class="breadcrumbs list-group">
+                <li class="list-group-item">
+                  <a href="#">Main Section</a>
+                </li>
+                <li class="list-group-item">
+                  <i class="fa-solid fa-chevron-right" id="subsection-arrow"></i>
+                  Subsection
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
     </div>
 `;
 }
