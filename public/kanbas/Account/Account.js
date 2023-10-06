3c68fbf4778b45c873187436ef90541afc350555
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-10-06 00:39:47 -0400

    began account page

--- public/kanbas/Account/Account.js
+++ public/kanbas/Account/Account.js
@@ -19,20 +19,42 @@ function Account() {
             <nav id="breadcrumbs">
               <ul class="breadcrumbs list-group">
                 <li class="list-group-item">
-                  <a href="#">Main Section</a>
-                </li>
-                <li class="list-group-item">
-                  <i class="fa-solid fa-chevron-right" id="subsection-arrow"></i>
-                  Subsection
+                  Rick Rubin&apos;s Profile
                 </li>
               </ul>
             </nav>
           </div>
         </div>
+        <div class="below-header-content">
+
+
         <div class="secondary-navigation">
           <div class="sn-list-container">
             ${SecondaryNavigation("Profile", "account")}
-          <div>
+          </div>
+        </div>
+        <div class="acc-main-container">
+          <div class="acc-main-content">
+              <div class="acc-profile-layout-primary">
+                <div class="acc-profile-logo-wrapper">
+                  <i class="fa-solid fa-user" id="profile-logo"></i>
+                </div>
+                <h2 id="profile-name">Rick Rubin</h2>
+                <div>
+                <h3 id="profile-header">Contact</h2>
+                <p>No registered services, you can add some on the <a href="#">settings</a> page.</p>
+                </div>
+                <div>
+                <h3 id="profile-header">Biography</h2>
+                <p>Rick Rubin, American record producer whose light touch and keen ear made him one of the most sought-after producers in popular music.</p>
+                </div class="profile-section">
+                <h3 id="profile-header">Links</h2>
+                <p><a href="https://www.youtube.com/@WebDevTV">YouTube</a></p>
+                </div>
+              </div>
+
+          </div>
+        </div>
         </div>
       </div>
     </div>
