c43a5b7dab26457bd3676eeb930f50b2334071bb
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-10-09 22:55:32 -0400

    created assignment edit page

---
+++ public/kanbas/Courses/Assignments/AssignmentEdit.js
@@ -0,0 +1,91 @@
+import SecondaryNavigation from "../../SecondaryNavigation/SecondaryNavigation.js"
+import KanbasNavigation from "../../KanbasNavigation/KanbasNavigation.js"
+
+function AssignmentEdit() {
+  return `
+    <div class="collapse collapse-horizontal" id="collapseKN">
+      <div class="d-xs-flex d-sm-flex d-md-none mobile-kanbas-nav">
+        <div class="kn-mobile-header-bar">
+          <a class="kn-mobile-header-bar-title" href="#">
+            <div class="kn-mobile-header-bar-icon-container">
+              <img class="logo-mark" src="../../../images/canvas_logo_single_mark.png" alt="..."/>
+            </div>
+            <div class="logo-label">KANBAS</div>
+          </a>
+          <a class="kn-mobile-header-bar-icon-container" id="xmark" data-bs-toggle="collapse" href="#collapseKN" role="button">
+            <i class="fa-solid fa-xmark" id="kn-xmark"></i>
+          </a>
+        </div>
+        ${KanbasNavigation("Courses", true)}
+      </div>
+    </div>
+    <div class="flex-row-container">
+      <div class="d-none d-md-flex kanbas-navigation">
+        <div class="logo-container">
+          <img class="logo-mark" src="../../../images/nu_logo.png" alt="..."/>
+        </div>
+        ${KanbasNavigation("Courses", false)}
+      </div>
+      <div class="main-content-wrapper">
+        <div class="d-none d-md-flex header-bar">
+          <i class="fa-solid fa-bars" id="menu-button"></i>
+          <div class="breadcrumb-wrapper">
+            <nav id="breadcrumbs">
+              <ul class="breadcrumbs list-group">
+                <li class="list-group-item">
+                  <a href="#">CS4550.19753.202410</a>
+                </li>
+                <li class="list-group-item">
+                  <i class="fa-solid fa-chevron-right" id="subsection-arrow"></i>
+                  <a href="/Kanbas/Courses/Assignments/index.html">Assignments</a>
+                </li>
+                <li class="list-group-item">
+                  <i class="fa-solid fa-chevron-right" id="subsection-arrow"></i>
+                  A# - Assignment Name
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div class="d-xs-flex d-sm-flex d-md-none mobile-header-bar">
+          <a class="mobile-header-bar-icon-container" data-bs-toggle="collapse" href="#collapseKN" role="button">
+            <i class="fa-solid fa-bars"></i>
+          </a>
+          <a class="mobile-header-title" data-bs-toggle="collapse" href="#collapseSN" role="button">
+            <div>CS4550.19753.202410</div>
+            <div>A# - Assignment Name</div>
+          </a>
+          <a class="mobile-header-bar-icon-container icon-toggle" data-bs-toggle="collapse" aria-expanded="false" href="#collapseSN" role="button">
+            <i class="fa-solid fa-chevron-down" id="chevron-down"></i>
+            <i class="fa-solid fa-xmark" id="xmark"></i>
+          </a>
+        </div>
+        <div class="collapse" id="collapseSN">
+          <div class="d-xs-block d-sm-block d-md-none mobile-secondary-nav">
+            ${SecondaryNavigation("Assignments", "courses", true)}
+          </div>
+        </div>
+        <div class="below-header-content">
+          <div class="d-none d-md-block secondary-navigation">
+            <div class="sn-list-container">
+              ${SecondaryNavigation("Assignments", "courses", false)}
+            </div>
+          </div>
+          <div class="assignment-main-content">
+            <div class="secondary-header-bar">
+              <div class="btns-row">
+                <button type="button" class="btn btn-primary ellipses"><i class="fa-solid fa-ellipsis-vertical"></i></button>
+                <div class="btn-row-publish-status">
+                  <i class="fa-solid fa-circle-check"></i>
+                  Published
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+`;
+}
+
+export default AssignmentEdit;
