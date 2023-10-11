0c33025da6b4d6e9c6d1446ae442441588d32948
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-10-10 20:17:07 -0400

    created grades page

--- public/kanbas/Courses/Grades/Grades.js
+++ public/kanbas/Courses/Grades/Grades.js
@@ -1,38 +1,71 @@
-import SecondaryNavigation from "../../SecondaryNavigation/SecondaryNavigation.js";
+import SecondaryNavigation from "../../SecondaryNavigation/SecondaryNavigation.js"
 import KanbasNavigation from "../../KanbasNavigation/KanbasNavigation.js"
 
 function Grades() {
   return `
+    <div class="collapse collapse-horizontal" id="collapseKN">
+      <div class="d-xs-flex d-sm-flex d-md-none mobile-kanbas-nav">
+        <div class="kn-mobile-header-bar">
+          <a class="kn-mobile-header-bar-title" href="#">
+            <div class="kn-mobile-header-bar-icon-container">
+              <img class="logo-mark" src="../../../images/canvas_logo_single_mark.png" alt="..."/>
+            </div>
+            <div class="logo-label">KANBAS</div>
+          </a>
+          <a class="kn-mobile-header-bar-icon-container" id="xmark" data-bs-toggle="collapse" href="#collapseKN" role="button">
+            <i class="fa-solid fa-xmark" id="kn-xmark"></i>
+          </a>
+        </div>
+        ${KanbasNavigation("Courses", true)}
+      </div>
+    </div>
     <div class="flex-row-container">
-      <div class="kanbas-navigation">
+      <div class="d-none d-md-flex kanbas-navigation">
         <div class="logo-container">
           <img class="logo-mark" src="../../../images/nu_logo.png" alt="..."/>
         </div>
-        <div>
-          ${KanbasNavigation("Courses")}
-        </div>
+        ${KanbasNavigation("Courses", false)}
       </div>
       <div class="main-content-wrapper">
-        <div class="header-bar">
+        <div class="d-none d-md-flex header-bar">
           <i class="fa-solid fa-bars" id="menu-button"></i>
           <div class="breadcrumb-wrapper">
             <nav id="breadcrumbs">
               <ul class="breadcrumbs list-group">
                 <li class="list-group-item">
-                  <a href="/Kanbas/Courses/Home/home.html">Main Section</a>
+                  <a href="/Kanbas/Courses/Home/home.html">CS4550.19753.202410</a>
                 </li>
                 <li class="list-group-item">
                   <i class="fa-solid fa-chevron-right" id="subsection-arrow"></i>
-                  Subsection
+                  Grades
                 </li>
               </ul>
             </nav>
           </div>
         </div>
-        <div class="secondary-navigation">
-          <div class="sn-list-container">
-            ${SecondaryNavigation("Grades", "courses")}
-          <div>
+        <div class="d-xs-flex d-sm-flex d-md-none mobile-header-bar">
+          <a class="mobile-header-bar-icon-container" data-bs-toggle="collapse" href="#collapseKN" role="button">
+            <i class="fa-solid fa-bars"></i>
+          </a>
+          <a class="mobile-header-title" data-bs-toggle="collapse" href="#collapseSN" role="button">
+            <div>CS4550.19753.202410</div>
+          </a>
+          <a class="mobile-header-bar-icon-container icon-toggle" data-bs-toggle="collapse" aria-expanded="false" href="#collapseSN" role="button">
+            <i class="fa-solid fa-chevron-down" id="chevron-down"></i>
+            <i class="fa-solid fa-xmark" id="xmark"></i>
+          </a>
+        </div>
+        <div class="collapse" id="collapseSN">
+          <div class="d-xs-block d-sm-block d-md-none mobile-secondary-nav">
+            ${SecondaryNavigation("Grades", "courses", true)}
+          </div>
+        </div>
+        <div class="below-header-content">
+          <div class="d-none d-md-block secondary-navigation">
+            <div class="sn-list-container">
+              ${SecondaryNavigation("Grades", "courses", false)}
+            </div>
+          </div>
         </div>
       </div>
     </div>
