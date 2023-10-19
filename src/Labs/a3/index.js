be5e5c341fb386efe1c5c4b055d414c3a05a6d5b
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-10-18 22:41:22 -0400

    finished lab up to 2.6

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,12 +1,16 @@
 import JavaScript from "./JavaScript";
 import PathParameters from "./PathParameters";
+import Classes from "./Classes";
+import Styles from "./Styles";
 
 function Assignment3() {
   return (
-      <div className="container">
+      <div>
         <h1>Assignment 3</h1>
-        <JavaScript/>
+        <Styles/>
+        <Classes/>
         <PathParameters/>
+        <JavaScript/>
       </div>
   );
 }
