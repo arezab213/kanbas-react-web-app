87bd881067df292cd1720f1aa80565c9b132fe3a
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-10-20 00:34:49 -0400

    removed account page

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -8,14 +8,11 @@ function Kanbas() {
   return (
       <div className="flex-row-container">
         <KanbasNavigation/>
-
         <Routes>
           <Route path="/" element={<Navigate to="Dashboard"/>}/>
-          <Route path="Account" element={<h1>Account</h1>}/>
           <Route path="Dashboard" element={<Dashboard/>}/>
           <Route path="Courses/:courseId/*" element={<Courses/>}/>
         </Routes>
-
       </div>
   );
 }
