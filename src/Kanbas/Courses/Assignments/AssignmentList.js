bb5db9da9255be42889924f5511e59af4410dff0
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-10-19 23:08:47 -0400

    finished 3.7 assignments screen

--- src/Kanbas/Courses/Assignments/AssignmentList.js
+++ src/Kanbas/Courses/Assignments/AssignmentList.js
@@ -1,6 +1,13 @@
 import {Link, useParams} from "react-router-dom";
 import React from "react";
 import db from "../../Database";
+import {
+  FaCircleCheck,
+  FaEllipsisVertical,
+  FaGripVertical,
+  FaPencil,
+  FaPlus
+} from "react-icons/fa6";
 
 function AssignmentList() {
   const {courseId} = useParams();
@@ -10,14 +17,61 @@ function AssignmentList() {
 
   return (
       <ul className="assignments-content list-group">
-        {courseAssignments.map((assignment) => (
-            <Link
-                key={assignment._id}
-                to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
-                className="list-group-item">
-              {assignment.title}
-            </Link>
-        ))}
+        <li className="list-group-item">
+          <div className="assignment-row-grip-container">
+            <FaGripVertical/>
+          </div>
+          <div className="assignment-row-header">
+            <div className="assignment-title">
+              Assignments
+            </div>
+          </div>
+          <div className="row-right-side-icons-container">
+            <div className="assignment-row-ellipsis-container">
+              <FaEllipsisVertical/>
+            </div>
+            <div className="assignment-row-icon-container plus">
+              <FaPlus/>
+            </div>
+            <span className="badge rounded-pill text-bg-primary">
+              40% of Total
+            </span>
+          </div>
+        </li>
+        {
+          courseAssignments.map((assignment) => (
+              <li className="list-group-item-secondary">
+                <div className="assignment-row-grip-container">
+                  <FaGripVertical/>
+                </div>
+                <div className="assignment-row-icon-container">
+                  <FaPencil/>
+                </div>
+                <div className="assignment-information">
+                  <div className="assignment-title">
+                    <Link key={assignment._id}
+                          to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}>
+                      {assignment.title}
+                    </Link>
+                  </div>
+                  <div className="assignment-period">
+                    {assignment.course}
+                  </div>
+                  <div className="assignment-deadline-points">
+                    <strong>{assignment._id}</strong> | 100 pts
+                  </div>
+                </div>
+                <div className="row-right-side-icons-container">
+                  <div className="assignment-row-ellipsis-container">
+                    <FaEllipsisVertical/>
+                  </div>
+                  <div className="assignment-row-icon-container">
+                    <FaCircleCheck/>
+                  </div>
+                </div>
+              </li>
+          ))
+        }
       </ul>
 
   )
