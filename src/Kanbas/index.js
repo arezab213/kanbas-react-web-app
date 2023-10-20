42f83b57ccd5415b48c4a1f473e9dfa128a63e33
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-10-20 04:19:38 -0400

    added mobile kanbas nav

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -3,17 +3,21 @@ import {Routes, Route, Navigate} from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import "./styles.css";
+import MobileKanbasNavigation from "./KanbasNavigation/MobileKanbasNavigation";
 
 function Kanbas() {
   return (
-      <div className="flex-row-container">
-        <KanbasNavigation/>
-        <Routes>
-          <Route path="/" element={<Navigate to="Dashboard"/>}/>
-          <Route path="Dashboard" element={<Dashboard/>}/>
-          <Route path="Courses/:courseId/*" element={<Courses/>}/>
-        </Routes>
-      </div>
+      <>
+        <MobileKanbasNavigation/>
+        <div className="flex-row-container">
+          <KanbasNavigation/>
+          <Routes>
+            <Route path="/" element={<Navigate to="Dashboard"/>}/>
+            <Route path="Dashboard" element={<Dashboard/>}/>
+            <Route path="Courses/:courseId/*" element={<Courses/>}/>
+          </Routes>
+        </div>
+      </>
   );
 }
 
