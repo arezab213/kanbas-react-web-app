231def0ae78c7d39ca62a8df4d2d0ef7a889b950
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-12-07 22:13:39 -0500

    fixed modal stylings and added module functionality to home

--- src/Kanbas/Courses/Home/index.js
+++ src/Kanbas/Courses/Home/index.js
@@ -8,13 +8,38 @@ import {
   FaRegCircleCheck, FaRightFromBracket, FaBars
 } from "react-icons/fa6";
 import {Link, useParams} from "react-router-dom";
-import db from "../../Database";
 import CourseNavigation from "../CourseNavigation";
 import {FaChevronRight} from "react-icons/fa";
+import {
+  MDBBtn,
+  MDBModal, MDBModalBody,
+  MDBModalContent,
+  MDBModalDialog, MDBModalFooter,
+  MDBModalHeader, MDBModalTitle
+} from "mdb-react-ui-kit";
+import {addModule, setModule} from "../Modules/modulesReducer";
+import React, {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
 
 function Home({course}) {
   const {courseId} = useParams();
-
+  const module = useSelector((state) => state.modulesReducer.module);
+  const dispatch = useDispatch();
+  const [moduleModal, setModuleModal] = useState(false);
+  const toggleOpen = (resetInitialModule = true) => {
+    setModuleModal(!moduleModal);
+    if (resetInitialModule) {
+      dispatch(
+          setModule({
+            name: "New Module 123",
+            description: "New Description"
+          }))
+    }
+  }
+  const saveButtonClick = (module) => {
+    dispatch(addModule(module));
+    toggleOpen(false);
+  };
   return (
       <div className="main-content-wrapper">
         <div className="d-none d-md-flex header-bar">
@@ -40,10 +65,54 @@ function Home({course}) {
               <button className="btn btn-primary ellipsis">
                 <FaEllipsisVertical/>
               </button>
-              <button className="btn btn-secondary add-module">
+              <button className="btn btn-secondary add-module" onClick={() => {
+                toggleOpen()
+              }}>
                 <FaPlus/>
                 Module
               </button>
+              <MDBModal open={moduleModal} setOpen={setModuleModal}
+                        tabIndex='-1'>
+                <MDBModalDialog>
+                  <MDBModalContent>
+                    <MDBModalHeader>
+                      <MDBModalTitle>Add Module</MDBModalTitle>
+                      <MDBBtn className='btn-close' color='none'
+                              onClick={() => toggleOpen(false)}>
+                      </MDBBtn>
+                    </MDBModalHeader>
+                    <MDBModalBody>
+                      <label htmlFor="moduleName" className="form-label">Module
+                        Title</label>
+                      <input value={module.name}
+                             className="form-control modal-input"
+                             id="moduleName"
+                             onChange={(e) => dispatch(setModule(
+                                 {...module, name: e.target.value}))}/>
+                      <label htmlFor="moduleDescription"
+                             className="form-label">Description</label>
+                      <textarea value={module.description}
+                                className="form-control modal-input"
+                                id="moduleDescription"
+                                onChange={(e) => dispatch(setModule(
+                                    {
+                                      ...module,
+                                      description: e.target.value
+                                    }))}/>
+                    </MDBModalBody>
+                    <MDBModalFooter>
+                      <button className="btn btn-secondary"
+                              onClick={() => toggleOpen(false)}>
+                        Cancel
+                      </button>
+                      <button className="btn btn-primary"
+                              onClick={() => saveButtonClick(
+                                  {...module, course: courseId})}>Add Module
+                      </button>
+                    </MDBModalFooter>
+                  </MDBModalContent>
+                </MDBModalDialog>
+              </MDBModal>
               <button className="btn btn-primary dropdown-toggle publish-all">
                 <FaRegCircleCheck/>
                 Publish All
--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -47,128 +47,132 @@ function Dashboard({
     }
   };
   return (
-      <div className="main-content-wrapper">
-        <div className="d-none d-md-flex header-bar">
-          <div className="breadcrumb-wrapper">
-            <nav id="breadcrumbs">
-              <ul className="breadcrumbs list-group">
-                <li className="list-group-item" id="dashboard-header">
-                  Dashboard
-                </li>
-              </ul>
-            </nav>
+      <>
+        <div className="main-content-wrapper">
+          <div className="d-none d-md-flex header-bar">
+            <div className="breadcrumb-wrapper">
+              <nav id="breadcrumbs">
+                <ul className="breadcrumbs list-group">
+                  <li className="list-group-item" id="dashboard-header">
+                    Dashboard
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
-        </div>
-        <div className="dashboard-main-content-wrapper">
-          <div className="header-bar">
-            Published Courses ({courses.length})
-            <button className="btn btn-primary" onClick={() => {
-              setModalTitles({header: "Add Course", button: "Add"})
-              toggleOpen();
-            }}>
-              Add Course
-            </button>
-            <MDBModal open={dashboardModal} setOpen={setDashboardModal}
-                      tabIndex='-1'>
-              <MDBModalDialog>
-                <MDBModalContent>
-                  <MDBModalHeader>
-                    <MDBModalTitle>{modalTitles.header}</MDBModalTitle>
-                    <MDBBtn className='btn-close' color='none'
-                            onClick={() => toggleOpen(false)}>
-                    </MDBBtn>
-                  </MDBModalHeader>
-                  <MDBModalBody>
-                    <label htmlFor="courseName" className="form-label">Course
-                      Name</label>
-                    <input value={course.name}
-                           className="form-control modal-input" id="courseName"
-                           onChange={(e) => setCourse(
-                               {...course, name: e.target.value})}/>
-                    <label htmlFor="courseNumber" className="form-label">Course
-                      Number</label>
-                    <input value={course.number}
-                           className="form-control modal-input"
-                           id="courseNumber"
-                           onChange={(e) => setCourse(
-                               {...course, number: e.target.value})}/>
-                    <label htmlFor="courseStartDate"
-                           className="form-label">Start</label>
-                    <input value={course.startDate}
-                           className="form-control modal-input"
-                           id="courseStartDate"
-                           type="date" onChange={(e) => setCourse(
-                        {...course, startDate: e.target.value})}/>
-                    <label htmlFor="courseEndDate"
-                           className="form-label">End</label>
-                    <input value={course.endDate}
-                           className="form-control modal-input"
-                           id="courseEndDate"
-                           type="date" onChange={(e) => setCourse(
-                        {...course, endDate: e.target.value})}/>
-                  </MDBModalBody>
-                  <MDBModalFooter>
-                    <button className="btn btn-secondary"
-                            onClick={() => toggleOpen(false)}>
-                      Cancel
-                    </button>
-                    <button className="btn btn-primary"
-                            onClick={saveButtonClick}>{modalTitles.button}
-                    </button>
-                  </MDBModalFooter>
-                </MDBModalContent>
-              </MDBModalDialog>
-            </MDBModal>
-          </div>
-          <div className="course-cards-container">
-            <div className="d-flex flex-row flex-wrap card-row">
-              {courses.map((course) => (
-                  <div className="card dashboard-course-card">
-                    <div className="dashboard-card-header">
-                      <div className="card-header-button" onClick={(event) => {
-                        event.preventDefault();
-                        deleteCourse(course._id);
-                      }}>
-                        <FaXmark/>
-                      </div>
-                    </div>
-                    <Link key={course._id} to={`/Kanbas/Courses/${course._id}`}>
-                      <div className="card-body dashboard-card">
-                        <h6 className="dashboard-card-title">
-                          {`${course._id} ${course.name}`}
-                        </h6>
-                        <div className="dashboard-card-subtitle">
-                          {`${course._id}.${course.number}`}
-                        </div>
-                        <div className="dashboard-card-course-term">
-                          {`${course.startDate} to ${course.endDate}`}
+          <div className="dashboard-main-content-wrapper">
+            <div className="header-bar">
+              Published Courses ({courses.length})
+              <button className="btn btn-primary" onClick={() => {
+                setModalTitles({header: "Add Course", button: "Add"})
+                toggleOpen();
+              }}>
+                Add Course
+              </button>
+            </div>
+            <div className="course-cards-container">
+              <div className="d-flex flex-row flex-wrap card-row">
+                {courses.map((course) => (
+                    <div className="card dashboard-course-card">
+                      <div className="dashboard-card-header">
+                        <div className="card-header-button"
+                             onClick={(event) => {
+                               event.preventDefault();
+                               deleteCourse(course._id);
+                             }}>
+                          <FaXmark/>
                         </div>
                       </div>
-                    </Link>
-                    <div className="dashboard-card-base">
-                      <Link to={`/Kanbas/Courses/${course._id}`}
-                            onClick={(event) => {
-                              event.preventDefault();
-                              setCourse(course);
-                              setModalTitles(
-                                  {header: "Edit Course", button: "Save"})
-                              toggleOpen(false);
-                            }}>
-                        <FaRegPenToSquare/>
+                      <Link key={course._id}
+                            to={`/Kanbas/Courses/${course._id}`}>
+                        <div className="card-body dashboard-card">
+                          <h6 className="dashboard-card-title">
+                            {`${course._id} ${course.name}`}
+                          </h6>
+                          <div className="dashboard-card-subtitle">
+                            {`${course._id}.${course.number}`}
+                          </div>
+                          <div className="dashboard-card-course-term">
+                            {`${course.startDate} to ${course.endDate}`}
+                          </div>
+                        </div>
                       </Link>
-                      <Link to={`/Kanbas/Courses/${course._id}`}>
-                        <FaBullhorn/>
-                      </Link>
-                      <Link to={`/Kanbas/Courses/${course._id}`}>
-                        <FaRegComments/>
-                      </Link>
+                      <div className="dashboard-card-base">
+                        <Link to={`/Kanbas/Courses/${course._id}`}
+                              onClick={(event) => {
+                                event.preventDefault();
+                                setCourse(course);
+                                setModalTitles(
+                                    {header: "Edit Course", button: "Save"})
+                                toggleOpen(false);
+                              }}>
+                          <FaRegPenToSquare/>
+                        </Link>
+                        <Link to={`/Kanbas/Courses/${course._id}`}>
+                          <FaBullhorn/>
+                        </Link>
+                        <Link to={`/Kanbas/Courses/${course._id}`}>
+                          <FaRegComments/>
+                        </Link>
+                      </div>
                     </div>
-                  </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        <MDBModal open={dashboardModal} setOpen={setDashboardModal}
+                  tabIndex='-1'>
+          <MDBModalDialog>
+            <MDBModalContent>
+              <MDBModalHeader>
+                <MDBModalTitle>{modalTitles.header}</MDBModalTitle>
+                <MDBBtn className='btn-close' color='none'
+                        onClick={() => toggleOpen(false)}>
+                </MDBBtn>
+              </MDBModalHeader>
+              <MDBModalBody>
+                <label htmlFor="courseName" className="form-label">Course
+                  Name</label>
+                <input value={course.name}
+                       className="form-control modal-input" id="courseName"
+                       onChange={(e) => setCourse(
+                           {...course, name: e.target.value})}/>
+                <label htmlFor="courseNumber" className="form-label">Course
+                  Number</label>
+                <input value={course.number}
+                       className="form-control modal-input"
+                       id="courseNumber"
+                       onChange={(e) => setCourse(
+                           {...course, number: e.target.value})}/>
+                <label htmlFor="courseStartDate"
+                       className="form-label">Start</label>
+                <input value={course.startDate}
+                       className="form-control modal-input"
+                       id="courseStartDate"
+                       type="date" onChange={(e) => setCourse(
+                    {...course, startDate: e.target.value})}/>
+                <label htmlFor="courseEndDate"
+                       className="form-label">End</label>
+                <input value={course.endDate}
+                       className="form-control modal-input"
+                       id="courseEndDate"
+                       type="date" onChange={(e) => setCourse(
+                    {...course, endDate: e.target.value})}/>
+              </MDBModalBody>
+              <MDBModalFooter>
+                <button className="btn btn-secondary"
+                        onClick={() => toggleOpen(false)}>
+                  Cancel
+                </button>
+                <button className="btn btn-primary"
+                        onClick={saveButtonClick}>{modalTitles.button}
+                </button>
+              </MDBModalFooter>
+            </MDBModalContent>
+          </MDBModalDialog>
+        </MDBModal>
+      </>
   );
 }
 
