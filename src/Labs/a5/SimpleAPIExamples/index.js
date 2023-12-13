c0a79ae42b5efd505834bde20bb9ab8838b6f74a
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-12-12 19:32:19 -0500

    completed 3.2 lab + extra credit

---
+++ src/Labs/a5/SimpleAPIExamples/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import EncodingParametersInURLs from "./EncodingParametersInURLs";
+import WorkingWithObjects from "./WorkingWithObjects";
+
+const SimpleAPIExamples = () => {
+  return (
+      <div>
+        <h2>Simple API Examples</h2>
+        <EncodingParametersInURLs/>
+        <WorkingWithObjects/>
+      </div>
+  );
+};
+
+export default SimpleAPIExamples;
