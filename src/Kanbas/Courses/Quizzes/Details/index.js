e2a3af7d910bf602cd2f875624a1d7f9d6722de8
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-12-17 11:43:42 -0500

    fixed publish course toggle

--- src/Kanbas/Courses/Quizzes/Details/index.js
+++ src/Kanbas/Courses/Quizzes/Details/index.js
@@ -5,7 +5,6 @@ import {
   FaBan,
   FaBars,
   FaChevronRight,
-  FaCircleCheck,
   FaEllipsisVertical, FaPencil, FaRegCircleCheck
 } from "react-icons/fa6";
 import {Link, useLocation, useNavigate, useParams} from "react-router-dom";
@@ -13,7 +12,6 @@ import {useDispatch, useSelector} from "react-redux";
 import {selectQuiz, updateQuiz} from "../quizzesReducer";
 import "../QuizDetailsEditor/index.css"
 import * as client from "../client";
-import {setAssignments} from "../../Assignments/assignmentsReducer";
 
 function QuizDetails({course}) {
   const {courseId, quizId} = useParams();
@@ -26,31 +24,13 @@ function QuizDetails({course}) {
   const navigate = useNavigate();
   const {isFormForEdit} = useLocation().state;
   const mobileHeaderInfo = {course: course, pageName: "Edit Quiz"};
- /* const findQuizById = async (quizId) => {
-    const response = await client.findQuizById(quizId)
-    dispatch(selectQuiz(response));
-  };
-  findQuizById(quizId)*/
   const handleUpdateQuizPublishStatus = async () => {
-    let currentlyPublished = quiz.published
-
-    dispatch(selectQuiz({...quiz, published: !currentlyPublished}));
-    const status = await client.updateQuiz(quiz);
-    dispatch(updateQuiz(quiz));
+    let currentlyPublished = quiz.published;
+    dispatch(selectQuiz({...quiz, published: !(currentlyPublished)}));
+    const status = await client.updateQuiz(
+        {...quiz, published: !(currentlyPublished)});
+    dispatch(updateQuiz({...quiz, published: !(currentlyPublished)}));
   };
-
-  /*const resetInitialQuiz = () => {
-    dispatch(selectQuiz({
-      title: "New Quiz",
-      points: 100,
-      description: "Quiz Description",
-      availableFromDate: "2023-01-10",
-      availableUntilDate: "2023-05-15",
-      dueDate: "2023-05-15",
-      published: false,
-      shuffleAnswers: false,
-    }))
-  }*/
   return (
       <div className="main-content-wrapper">
         <MobileHeader obj={mobileHeaderInfo}/>
@@ -94,7 +74,8 @@ function QuizDetails({course}) {
                   Preview
                 </button>
                 <button type="button" className="btn btn-primary"
-                        id="quiz-publish-btn" onClick={handleUpdateQuizPublishStatus}>
+                        id="quiz-publish-btn"
+                        onClick={handleUpdateQuizPublishStatus}>
                   {quiz.published ? <FaRegCircleCheck/> : <FaBan/>}
                   {quiz.published ? "Published" : "Unpublished"}
                 </button>
